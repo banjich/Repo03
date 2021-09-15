74e4766480e45f672cde1589cca8200d42cd6c3b
Adnan <banjich@gmail.com>
2021-09-15 22:13:53 +0200

    Added file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+//This is my first comment
+
+/*This is my first multi-line comment
+which is very veryyyy veryyyyy long*/
+
+}
+
+console.log(main());
+
+module.exports = main;
+
